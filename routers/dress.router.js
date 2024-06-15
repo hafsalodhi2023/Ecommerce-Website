58e095c813978630b3f5a66701adc14a60ebd211
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-06-15 16:28:42 +0500

    product getting api completed

---
+++ routers/dress.router.js
@@ -0,0 +1,9 @@
+import dressController from "../controllers/get-dress.controller.js";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/getall", dressController.getAll);
+// .get("/get/:id", dressController.getOne);
+
+export default router;
