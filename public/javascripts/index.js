1d2b0bc73115a0a55a61e3ca5925dddadba9abad
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-06-19 16:16:24 +0500

    sidebar has completed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,20 @@
 const hamburger = document.querySelector("#hamburger");
 const sidebar = document.querySelector("#sidebar");
 const close_btn = document.querySelector("#close-btn");
+const shop_btn = document.querySelector("#shop-btn");
+const shop_subs = document.querySelectorAll(".shop_sub");
+
+shop_subs.forEach((shop_sub) => {
+  shop_btn.addEventListener("click", () => {
+    if (shop_sub.classList.contains("hidden")) {
+      shop_sub.classList.remove("hidden");
+      shop_sub.classList.add("flex");
+    } else if (shop_sub.classList.contains("flex")) {
+      shop_sub.classList.remove("flex");
+      shop_sub.classList.add("hidden");
+    }
+  });
+});
 
 hamburger.addEventListener("click", (e) => {
   sidebar.className =
@@ -9,5 +23,5 @@ hamburger.addEventListener("click", (e) => {
 
 close_btn.addEventListener("click", () => {
   sidebar.className =
-    "fixed flex flex-col pt-20 top-0 left-[-100%] w-full h-dvh bg-color_1  items-center justify-center";
+    "fixed flex flex-col pt-20 top-0 left-[-100%] w-full h-dvh bg-color_1 items-center justify-center";
 });
